6a044d2cf5d40c08b28cdfd9dade02bba4846733
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2018-08-08 10:54:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var fundo = document.querySelector('main')
 var warning = document.querySelector('.warning')
 
-setTimeout(function() {
+window.onload = function() {
     warning.classList.add('warning_show_up')
     fundo.classList.add('main_blur')
-}, 1500)
+}
